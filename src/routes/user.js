6708fbc565134bc65442d0bac761cf3654214df7
JulioRugolo <juliorugolo@gmail.com>
2023-07-14 20:00:23 -0300

    criando rota para /user/:id

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createUser, getAllUsers } = require('../controllers/user');
+const { createUser, getAllUsers, getUserById } = require('../controllers/user');
 const validateUser = require('../middleware/validateUser');
 const tokenValidation = require('../middleware/tokenValidator');
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post('/', validateUser, createUser);
 router.get('/', tokenValidation, getAllUsers);
+router.get('/:id', tokenValidation, getUserById);
 
 module.exports = router;
